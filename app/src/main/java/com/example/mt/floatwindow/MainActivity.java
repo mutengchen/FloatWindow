6c173c11170618aec9161ce62534bcbdbe1558c3
yourname <youremail>
2019-04-17 17:30:48 +0800

    zzzw

--- app/src/main/java/com/example/mt/floatwindow/MainActivity.java
+++ app/src/main/java/com/example/mt/floatwindow/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mt.floatwindow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.floatbutton.FloatDialogWindow;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
